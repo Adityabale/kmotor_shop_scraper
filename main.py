3fa086f7d1f84480f3d40b45b65f49d63a605876
Aditya Balekundri <balekundri@segmenty.com>
2024-01-09 18:16:10 +0530

    Remove bs4 and use only selenium

--- main.py
+++ main.py
@@ -27,11 +27,10 @@ class KMotorShopScraper:
         self.driver: Optional[WebDriver] = None
         self.session = None
 
-    def get_models_data(self, headers: dict):
+    def get_models_data(self):
         """Gets all the spare parts list for models from the main page."""
-        actions = ActionChains(self.driver)
-        retry = int()
-        model_count = int()
+        model_count = 0
+        # actions = ActionChains(self.driver)
         wait5s = WebDriverWait(self.driver, 5)
         dataframes = list()
         self.driver.get(self.main_page)
@@ -48,39 +47,29 @@ class KMotorShopScraper:
                     model_link = models_divs[i].find_element(By.TAG_NAME, 'a').get_attribute('href')
                     self.driver.get(model_link)
                     time.sleep(2)
-                    part_no_links = wait5s.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'table.searchtable a')))
+                    part_no_links = wait5s.until(
+                        EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'table.searchtable a')))
                     for idx, link in enumerate(part_no_links):
                         part_no_links = wait5s.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,
                                                                                           'table.searchtable a')))
                         self._scroll_to(self.driver, part_no_links[idx])
-                        part_no = part_no_links[idx].get_attribute('innerText').strip()
-                        # self.driver.get(part_no_links[idx].get_attribute('href'))
-                        # time.sleep(2)
-                        response = requests.get(part_no_links[idx].get_attribute('href'), headers=headers)
                         time.sleep(2)
-                        html = response.text
-                        # soup = BeautifulSoup(html, 'html5lib')
-                        try:
-                            tables = pd.read_html(html)
-                            df = pd.DataFrame(tables[0])
-                            df['Model'] = model_name
-                            dataframes.append(df)
-                            # print(df)
-                            print(f"Done: {model_name}\t {part_no}")
-                            # self.driver.back()
-                            # time.sleep(2)
-                            model_count += 1
-                        except ValueError: #if no table
-                            pass
+                        part_no = part_no_links[idx].get_attribute('innerText').strip()
+                        self.driver.get(part_no_links[idx].get_attribute('href'))
+                        time.sleep(2)
+                        tables = pd.read_html(self.driver.page_source)
+                        df = pd.DataFrame(tables[0])
+                        df['Model'] = model_name
+                        dataframes.append(df)
+                        print(f"Done: {model_name}\t {part_no}")
+                        self.driver.back()
+                        time.sleep(2)
                     self.driver.back()
                     time.sleep(2)
-            except requests.exceptions.ConnectionError:
-                retry += 1
-                time.sleep(60*retry)
-                continue
-
+                    model_count += 1
+            except exceptions.TimeoutException:
+                model_count += 1
         self._concat_get_csv(dataframes)
-        time.sleep(2)
 
     def _scroll_to(self, driver: WebDriver, element: WebElement):
         """Scroll to the given WebElement.
@@ -108,13 +97,6 @@ class KMotorShopScraper:
 
 
 def main():
-    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-    REQUEST_HEADER = {
-
-        'User-Agent': USER_AGENT,
-        'Accept-Language': 'en-US, en;q=0.5',
-
-    }
     CHROME_DRIVER = os.environ['CHROME_DRIVER_PATH']
     service = ChromeService(executable_path=CHROME_DRIVER)
     # chrome_options = ChromeOptions()
@@ -125,7 +107,7 @@ def main():
 
     scraper = KMotorShopScraper()
     scraper.driver = driver
-    scraper.get_models_data(headers=REQUEST_HEADER)
+    scraper.get_models_data()
 
 
 if __name__ == '__main__':
